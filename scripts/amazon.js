476e44e2ab4aa8627066045b87b053f1992b9d5e
Lesuuh <lesuuh01@gmail.com>
2024-12-22 02:00:44 +0100

    make add to cart button interative

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,4 @@
-const products = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
-let productsHTML = ""
+let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
@@ -74,20 +41,45 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart">
             <img src="images/icons/checkmark.png">
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
+          }">
             Add to Cart
           </button>
         </div>
     `;
 });
 
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-console.log(productsHTML)
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    // let matchingItem = undefined;
 
+    // cart.forEach((item) => {
+    //   if (productName === item.productName) {
+    //     matchingItem = item;
+    //   }
+    // });
 
-document.querySelector(".js-products-grid").innerHTML = productsHTML
+    // if (matchingItem) {
+    //   matchingItem.quantity += 1;
+    // } else {
+    //   cart.push({ productName: productName, quantity: 1 });
+    // }
+
+    const productInCart = cart.find((item) => item.productId === productId);
+    if (productInCart) {
+      productInCart.quantity++;
+    } else {
+      cart.push({ productId: productId, quantity: 1 });
+    }
+    console.log(cart);
+  });
+});
